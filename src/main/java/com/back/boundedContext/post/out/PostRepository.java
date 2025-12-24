07943f24ec0994c21c9fc7c8cd864826416a4906
sohee52 <wneh2023@naver.com>
2025-12-24 15:22:46 +0900

    0021

--- src/main/java/com/back/boundedContext/post/out/PostRepository.java
+++ src/main/java/com/back/boundedContext/post/out/PostRepository.java
@@ -1,13 +1,7 @@
 package com.back.boundedContext.post.out;
 
 import com.back.boundedContext.post.domain.Post;
-import com.back.boundedContext.post.domain.PostMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface PostRepository extends JpaRepository<Post, Integer> {
-    public interface PostMemberRepository extends JpaRepository<PostMember, Long> {
-        Optional<PostMember> findByUsername(String username);
-    }
 }
